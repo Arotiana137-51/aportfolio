43cebae3d0779992bb8a9f694fd0f0bc049444a0
Arotiana137-51 <85313952+Arotiana137-51@users.noreply.github.com>
2023-03-07 19:08:20 +0300

    Update contact.js
    console.log

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,7 @@
 // Librairie
 
 export default function handler(req, res) {
+	console.log("entree dans le service de sendmail");
   if (req.method !== "POST") {
     res.status(405).json({ message: "INVALID_METHOD" });
     return;
